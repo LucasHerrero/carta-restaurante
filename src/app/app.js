98e0ba937067fd4476eb15684980c6a490ef3370
Lucas Herrero <lucasherrero2010@hotmail.com>
2024-02-13 14:26:22 +0100

    Refactor app.js and style.css files

--- src/app/app.js
+++ src/app/app.js
@@ -2,13 +2,33 @@ const api = 'https://65c668d8e5b94dfca2e17f0b.mockapi.io/api/v1/comida_menu'; //
 const platos = document.getElementById('platos');
 var estructuraHTML = `
 <div class="plato">
-    <h3>Titulo-plato</h3>
-    <ul>
+    <h2>Titulo-plato</h2>
+    
     <img class="imgComida" src="Fotografia" alt="Fotografia_comida">
-    <li>Ingrediente</li>
-    <img class="imgInto" src="Intolerancias" alt="Fotografia_intolerancia">
-    <li>Precio</li>
-     </ul>
+    
+    <ul class="Ingr">
+    <h3> 
+    Ingredientes:
+    </h3>
+    
+    </ul>
+
+    <div class="sp-bt">
+    
+     
+        <div class="ING">
+    
+            ing
+       
+        </div>
+        
+
+    </div>
+
+   <p>Intolerancias</p>
+   
+    <li><span>Precio €</span></li>
+    
 </div>
 `;
 
@@ -19,29 +39,14 @@ function apiConnection() {
         .then(response => response.json())
         .then(data => {
             data.forEach(plato => {
-                let celiacos = '/src/assets/img/celiaco.png';
-                let diabeticos = '/src/assets/img/diabetes.png';
-                let veganos = '/src/assets/img/vegano.png';
 
-                arr = new Array();
-
-                if (plato.intolerancias.length == 0) {
-
-                } else if (plato.intolerancias.includes('Celiacos')) {
-                    arr.push(celiacos);
-                } else if (plato.intolerancias.includes('Diabeticos')) {
-                    arr.push(diabeticos);
-                }
-                else if (plato.intolerancias.includes('Veganos')) {
-                    arr.push(veganos);
-                }
 
                 let platoHTML = estructuraHTML
                     .replace('Titulo-plato', plato.nombre)
                     .replace('Fotografia', plato.fotografia)
                     .replace('Nombre', plato.nombre)
-                    .replace('Ingrediente', plato.ingredientes.join(', '))
-                    .replace('Intolerancias', arr.join(', '))
+                    .replace('ing', plato.ingredientes.join(', '))
+                    .replace('Intolerancias', plato.intolerancias.join(', '))
                     .replace('Precio', plato.precio);
                 platos.innerHTML += platoHTML;
             });
